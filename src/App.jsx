4677275d3a55d7e82161b032499148e92e550188
christian.remillard <christian.remillard@gmail.com>
2024-11-30 15:15:30 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,82 @@
 import { useEffect, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Bounds, OrbitControls, useGLTF, useAnimations, useScroll, ScrollControls, SoftShadows } from "@react-three/drei"
-import { EffectComposer, SSAO, SMAA, Selection, Outline } from "@react-three/postprocessing"
-import Model from './components/lock-21-mockup/lock-21-mockup'
-import {zones} from './zones'
+import { Bounds, OrbitControls, CameraControls, GizmoHelper, GizmoViewport, useCursor, useGLTF, useAnimations, SoftShadows } from '@react-three/drei'
+import { EffectComposer, SSAO, SMAA, Selection, Outline } from '@react-three/postprocessing'
+import Model from './components/lock-21-mockup/lock-21-mockup-2.jsx'
+import { zones } from './zones'
 import './App.css'
 
-// function Model(props) {
-//   const scroll = useScroll()
-//   const { nodes, materials, animations } = useGLTF("/lock-21-mockup.glb")
-//   const { ref, actions } = useAnimations(animations)
-//   // useEffect(() => void (actions.jump.reset().play().paused = true), [])
-//   // useFrame(() => (actions.jump.time = actions.jump.getClip().duration * scroll.offset))
-//   return (
-//     <group {...props} ref={ref}>
-//       <primitive object={nodes.mixamorigHips} />
-//       <skinnedMesh castShadow receiveShadow geometry={nodes.Ch03.geometry} material={materials.Ch03_Body} skeleton={nodes.Ch03.skeleton} />
-//     </group>
-//   )
-// }
-
 export function App() {
-  const [overed, over] = useState(null)
+  const [hovered, hover] = useState(null)
 
-  function onPointerOver(event){
+  useCursor(hovered)
+
+  function onPointerOver(event) {
+    event.stopPropagation()
     const mesh = event.object
-    console.log('[onPointerOver]', mesh)
-    // over(mesh)
+    console.log('[onPointerOver]', mesh.name)
     // console.log(mesh.name)
     // // console.log(zones)
 
-    // if (zones.includes(mesh.name)){
-    //   console.log('yep')
-    // }
+    if (zones.includes(mesh.name)) {
+      // console.log('yep')
+      hover(mesh.name)
+    }
   }
 
-  function onPointerOut(event){
+  function onPointerOut(event) {
     const mesh = event.object
+    console.log('[onPointerOut]', mesh.name)
 
-    if (mesh === overed) {
-      over(null)
+    if (mesh.name === hovered) {
+      hover(null)
     }
-
-    console.log('[onPointerOut]', mesh)
   }
 
   useEffect(() => {
-    console.log('[overed]', overed)
-  }, [overed])
+    if (!hovered) {
+      return
+    }
+    console.log('[hovered]', hovered)
+  }, [hovered])
 
   return (
     <>
-      <Canvas shadows gl={{antialias: true}} camera={{ position: [.5, -.25, 1.5], fov: 50 }}>
+      <Canvas shadows gl={{ antialias: true }} camera={{ position: [-0.5, 1.5, 1], fov: 20 }}>
         <color attach='background' args={['#2c5828']} />
         {/* <fog attach="fog" args={["#f0f0f0", 0, 20]} /> */}
-        <fog attach="fog" args={["#2c5828", 0, 5]} />
+        {/* <fog attach='fog' args={['#2c5828', 0, 12]} /> */}
         <ambientLight intensity={0.75} />
-        <directionalLight intensity={1.5} position={[-5, -5, 10]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
+        <directionalLight intensity={1.5} position={[5, 10, 5]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
         <Selection>
-          <EffectComposer multisampling={0} autoClear={false}>
-            <SSAO radius={0.05} intensity={150} luminanceInfluence={0.5} color="black" />
-            <Outline visibleEdgeColor="#ffdd33" hiddenEdgeColor="#190a05" blur width={1000} edgeStrength={70} />
+          <EffectComposer multisampling={0} autoClear={false} enableNormalPass={true}>
+            <SSAO radius={0.05} intensity={150} luminanceInfluence={0.5} color='black' />
+            <Outline visibleEdgeColor='#ffdd33' hiddenEdgeColor='#190a05' blur width={1000} edgeStrength={70} />
             <SMAA />
           </EffectComposer>
           <Bounds fit clip margin={1.2} damping={0}>
-            <Model position={[0, 0, 0]} rotation={[0, 0, 0]} scale={0.01} onPointerOver={onPointerOver} onPointerOut={onPointerOut} />
+            <Model position={[0, 0, 0]} rotation={[0, 0, 0]} scale={0.01} onPointerOver={onPointerOver} onPointerOut={onPointerOut} hovered={hovered} />
           </Bounds>
         </Selection>
-        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI} />  
+        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI} />
+        {/* <CameraControls /> */}
+        <GizmoHelper
+          alignment='bottom-right' // widget alignment within scene
+          margin={[80, 80]} // widget margins (X, Y)
+        >
+          <GizmoViewport axisColors={['red', 'green', 'blue']} labelColor='black' />
+          {/* alternative: <GizmoViewcube /> */}
+        </GizmoHelper>
       </Canvas>
-      <div style={{
-        position: 'absolute',
-        bottom: 0,
-        padding: '.5rem'
-        }}><strong>hover: {overed}</strong></div>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          padding: '.5rem',
+        }}
+      >
+        <strong>hover: {hovered}</strong>
+      </div>
     </>
   )
 }
--- src/zones.js
+++ src/zones.js
@@ -1,3 +1,3 @@
 export const zones = [
-    'zone_8_-_lens_corrected_-_mesh_-_high_quality_200K_faces'
+    'zone_8'
 ]
